f99fa612456340df536d8c89726fc5667a320b14
Deven Rathore <hyguyz@outlook.com>
2023-05-12 19:55:41 +0530

    vue

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# VueportfolioBootstrap
-a portfolio website built using Vue and Bootstrap
+# vuex-example
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <navigation/>
+    <router-view></router-view>
+    <Footer/>
+    
+  </div>
+</template>
+
+<script>
+import Navigation from './components/includes/nav.vue'
+import Footer from './components/includes/footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navigation,
+    Footer
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
